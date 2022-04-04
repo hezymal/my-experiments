56cc00543ccdcf4288be3aaede46271ce2b7b493
hezymal <hezymal@gmail.com>
2022-04-04 18:23:48 +0300

    Add pwa

---
+++ pwa/README.md
@@ -0,0 +1,8 @@
+# progressive web application (pwa) sketch
+
+sources
+- https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+- https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+- https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Installable_PWAs
+- https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
+- https://habr.com/ru/company/2gis/blog/345552/
---
+++ pwa/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    const handleClick = () => {
+        fetch("/img/background/img.png");
+    };
+
+    return (
+        <div className="App" onClick={handleClick}>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
